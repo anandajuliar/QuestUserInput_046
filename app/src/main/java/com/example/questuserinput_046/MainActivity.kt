42513b08b2d8b666bd9fe1f13a1c92918c211d82
julia <anandajulia001@gmail.com>
2025-11-02 10:24:22 +0700

    membuat file n fun

--- app/src/main/java/com/example/questuserinput_046/MainActivity.kt
+++ app/src/main/java/com/example/questuserinput_046/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestUserInput_046Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    FormatDataDiri(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
