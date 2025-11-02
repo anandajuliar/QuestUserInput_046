307774fbee0cdaa9224e6d8a5969b904fc977188
julia <anandajulia001@gmail.com>
2025-11-02 10:54:08 +0700

    membuat kolom

--- app/src/main/java/com/example/questuserinput_046/Form.kt
+++ app/src/main/java/com/example/questuserinput_046/Form.kt
@@ -60,4 +60,8 @@ fun RegistrationForm(modifier: Modifier = Modifier) {
     var jenis by remember { mutableStateOf(value = "") }
 
     val gender:List<String> = listOf("Laki-laki", "Perempuan")
+
+    Column(modifier = Modifier.padding(top = 50.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally){}
 }
