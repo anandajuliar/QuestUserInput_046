edaf1af347f43b5c7d06ef69c25dee577d560749
julia <anandajulia001@gmail.com>
2025-11-02 10:45:53 +0700

    membuat var (mengambil dr praktikum)

--- app/src/main/java/com/example/questuserinput_046/Form.kt
+++ app/src/main/java/com/example/questuserinput_046/Form.kt
@@ -1,9 +1,63 @@
 package com.example.questuserinput_046
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.res.stringResource
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 @Composable
 fun RegistrationForm(modifier: Modifier = Modifier) {
+    var textNamaLengkap by remember { mutableStateOf(value = "") }
+    var textKotaAsal by remember { mutableStateOf(value = "") }
+    var textTanggalLahir by remember { mutableStateOf(value = "") }
+    var textRT by remember { mutableStateOf(value = "") }
+    var textRW by remember { mutableStateOf(value = "") }
+    var textUmur by remember { mutableStateOf(value = "") }
+    var textJK by remember { mutableStateOf(value = "") }
 
+    var nama by remember { mutableStateOf(value = "") }
+    var asal by remember { mutableStateOf(value = "") }
+    var tanggal by remember { mutableStateOf(value = "") }
+    var rt by remember { mutableStateOf(value = "") }
+    var rw by remember { mutableStateOf(value = "") }
+    var umur by remember { mutableStateOf(value = "") }
+    var jenis by remember { mutableStateOf(value = "") }
+
+    val gender:List<String> = listOf("Laki-laki", "Perempuan")
 }
